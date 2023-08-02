098f98d6029d9b55b2bc5af758d600375ce7fba3
Partha Saradhi Konda <parthasaradhi1992@gmail.com>
2023-08-02 11:27:00 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-# Define the output JSON object
 json='{}'
 
-# Loop through all .tf files
 for file in $(find . -name "*.tf")
 do
-  echo $file
-  # Extract 'default_tags' block after 'provider "azurerm"' block
-  block=$(sed -n '/provider "azurerm"/,/}/p' $file | sed -n '/default_tags/,/}/p')
-  echo $block
-  # Use awk to get each line with a = and then parse it with sed
+  # Get the line numbers for the start and end of the locals block
+  start=$(grep -n 'locals {' $file | cut -d : -f 1)
+  end=$(grep -n '}' $file | cut -d : -f 1 | awk -v start=$start '$1 > start {print $1; exit}')
+
+  # If 'locals {' is not found in the file, skip to next file
+  if [[ -z $start ]]; then
+    # echo "No 'locals {' found in $file, skipping..."
+    continue
+  fi
+
+  # Extract the default_tags block within the locals block
+  block=$(sed -n "${start},${end}p" $file | sed -n '/default_tags {/,/}/p')
+
   while read -r line
   do
-    if [[ $line == *=* ]] && [[ $line != *"tags"* ]]; then
+    if [[ $line == *=* ]] && [[ $line != *"default_tags"* ]]; then
       key=$(echo $line | awk -F'=' '{print $1}' | sed 's/ //g' | sed 's/"//g')
       value=$(echo $line | awk -F'=' '{print $2}' | sed 's/ //g' | sed 's/"//g' | sed 's/,//g')
       json=$(echo $json | jq --arg key "$key" --arg value "$value" '. + {($key): $value}')
@@ -21,8 +27,11 @@ do
   done <<< "$block"
 done
 
-# Export the JSON object as a shell variable
+
+
+# Print the JSON object as a shell variable to a file
 echo $json
-# Now you can use the $YOR_SIMPLE_TAGS variable in yor
 export YOR_SIMPLE_TAGS=$json
+# Now you can source this file in GitLab CI/CD pipeline script
+# source yor_tags.sh
 yor tag --tag-groups simple --directory .
